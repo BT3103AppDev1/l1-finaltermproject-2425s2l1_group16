fb0cd3c33ae94700f78e79a2ddb2fdd29b6cec34
Queena <148352740+scoopydoooKing@users.noreply.github.com>
2025-03-26 22:21:26 +0800

    rough logic for upvoting and reporting, yet to include updates for contribution points

--- src/views/InterviewQues.vue
+++ src/views/InterviewQues.vue
@@ -24,30 +24,74 @@ export default {
   },
   mounted() {
     async function display() {
-  
       // this will be within each interview ques in the db
+      // mite need to change the code to get rl time data
       let allUpvotes = await getDocs(collection(db, "Upvotes"));
       this.upvote_counter = allUpvotes.data().count();
 
-      allUpvotes.forEach((docs) => {
-        let documentData = docs.data();
-      });
-
       let allReports = await getDocs(collection(db, "Reports"));
       this.report_counter = allReports.data().count();
+      // need to check if report is more than 9
+      if (this.report_counter >= 9) {
+        //update status to remove question from user view
+      }
+    }
+    display();
 
-      allReports.forEach((docs) => {
+    async function increment_upvote() {
+      let allUpvotes = await getDocs(collection(db, "Upvotes"));
+      allUpvotes.forEach(async (docs) => {
         let documentData = docs.data();
+        //verify the same name is used in firebase
+        let username = documentData.username;
+
+        //check if current user is in the list
+        if (username == "user") {
+          //delete or remove from list
+          await deleteDoc(doc(db, "Upvotes", username));
+        } else {
+          // add name into list
+          try {
+            const docRef = await setDoc(doc(db, "Upvotes", username), {
+              User: username,
+              //include upvote time ?
+            });
+          } catch (error) {
+            console.error("Error adding document: ", error);
+          }
+        }
+        // need to check if value will be updated
+        display();
+      });
+    }
+
+    async function increment_report() {
+      let allReports = await getDocs(collection(db, "Reports"));
+      allReports.forEach(async (docs) => {
+        let documentData = docs.data();
+        //verify the same name is used in firebase
+        let username = documentData.username;
+
+        //check if current user is in the list
+        if (username == "user") {
+          //delete or remove from list
+          await deleteDoc(doc(db, "Reports", username));
+        } else {
+          // add name into list
+          try {
+            const docRef = await setDoc(doc(db, "Reports", username), {
+              User: username,
+              //include report time ?
+            });
+          } catch (error) {
+            console.error("Error adding document: ", error);
+          }
+        }
+        // need to check if value will be updated
+        display();
       });
     }
   },
-  methods: {
-    increment_upvote() {
-      this.upvote_counter += 1;
-    },
-    increment_report() {
-      this.upvote_report += 1;
-    },
-  },
+  methods: {},
 };
 </script>
