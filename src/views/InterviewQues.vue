33f5595382657d58596c4a6e7b61c6e7893c892d
Queena <148352740+scoopydoooKing@users.noreply.github.com>
2025-03-29 22:47:44 +0800

    contributions pts can be added

--- src/views/InterviewQues.vue
+++ src/views/InterviewQues.vue
@@ -4,14 +4,18 @@
 </template>
 
 <script>
+import { Filter } from "bad-words";
 import firebaseApp from "../firebase.js";
 import { getFirestore } from "firebase/firestore";
 import {
   doc,
   setDoc,
   collection,
+  getDoc,
   getDocs,
+  updateDoc,
   deleteDoc,
+  increment,
 } from "firebase/firestore";
 const db = getFirestore(firebaseApp);
 
@@ -23,75 +27,94 @@ export default {
     };
   },
   mounted() {
-    async function display() {
+    this.display();
+  },
+  methods: {
+    async display() {
       // this will be within each interview ques in the db
       // mite need to change the code to get rl time data
-      let allUpvotes = await getDocs(collection(db, "Upvotes"));
+      const allUpvotes = await getDocs(collection(db, "Upvotes"));
       this.upvote_counter = allUpvotes.size;
 
-      let allReports = await getDocs(collection(db, "Reports"));
+      const allReports = await getDocs(collection(db, "Reports"));
       this.report_counter = allReports.size;
       // need to check if report is more than 9
       if (this.report_counter >= 9) {
         //update status to remove question from user view
       }
-    }
-    display();
+    },
 
-    async function increment_upvote() {
-      let allUpvotes = await getDocs(collection(db, "Upvotes"));
-      allUpvotes.forEach(async (docs) => {
-        let documentData = docs.data();
-        //verify the same name is used in firebase
-        let username = documentData.username;
-        //check if current user is in the list
-        if (username == "user") {
-          //delete or remove from list
-          await deleteDoc(doc(db, "Upvotes", username));
+    async increment_upvote() {
+      let current = "user";
+      const userRef = doc(db, "Upvotes", current);
+      try {
+        const docSnap = await getDoc(userRef);
+        if (docSnap.exists()) {
+          await deleteDoc(userRef);
+          //delete points if they un upvote ?
         } else {
-          // add name into list
-          try {
-            const docRef = await setDoc(doc(db, "Upvotes", username), {
-              User: username,
-              //include upvote time ?
+          const docRef = await setDoc(doc(db, "Upvotes", current), {
+            username: current,
+          });
+          //getting user info & adding points
+          const pointsRef = doc(db, "Users", "insights_me");
+          await updateDoc(pointsRef, {
+            contribution_pts: increment(1),
+          });
+        }
+      } catch (error) {
+        console.error("Error adding document", error);
+      }
+
+      this.display();
+    },
+
+    async increment_report() {
+      let current = "user";
+      const userRef = doc(db, "Reports", current);
+      try {
+        const docSnap = await getDoc(userRef);
+        if (docSnap.exists()) {
+          await deleteDoc(userRef);
+        } else {
+          const docRef = await setDoc(doc(db, "Reports", current), {
+            username: current,
+          });
+        }
+      } catch (error) {
+        console.error("Error adding document", error);
+      }
+
+      this.display();
+    },
+
+    async quality_check() {
+      const filter = new Filter();
+      const allQuestions = await getDocs(collection(db, "Interview_Questions"));
+      //checking each question
+      allQuestions.forEach(async (docs) => {
+        let documentData = docs.data();
+        if (documentData.status == "Checked") {
+          return;
+        } else {
+          const filter = new Filter();
+          if (!filter.isProfane(documentData.question)) {
+            
+            //getting user info & adding points
+            // get documentData.name instead but using placeholder for now
+            const uploader = "insights_me"
+            const pointsRef = doc(db, "Users", uploader);
+            const updatePointsDoc = await updateDoc(pointsRef, {
+              contribution_pts: increment(5),
             });
-          } catch (error) {
-            console.error("Error adding document: ", error);
+            const questionRef = doc(db, "Interview_Questions", docs.id);
+            updateDoc(questionRef, {
+              status: "Checked",
+            });
           }
         }
-         
-        // need to check if value will be updated
-        display();
       });
-    }
-
-    async function increment_report() {
-      let allReports = await getDocs(collection(db, "Reports"));
-      allReports.forEach(async (docs) => {
-        let documentData = docs.data();
-        //verify the same name is used in firebase
-        let username = documentData.username;
-
-        //check if current user is in the list
-        if (username == "user") {
-          //delete or remove from list
-          await deleteDoc(doc(db, "Reports", username));
-        } else {
-          // add name into list
-          try {
-            const docRef = await setDoc(doc(db, "Reports", username), {
-              User: username,
-              //include report time ?
-            });
-          } catch (error) {
-            console.error("Error adding document: ", error);
-          }
-        }
-        // need to check if value will be updated
-        display();
-      });
-    }
+    },
   },
-  methods: {},
 };
 </script>
