9edc27c0c1ead6cdef3f74eb516f7647124e4231
Bryan Lam <bryanlam02@gmail.com>
2025-04-04 22:29:43 +0800

    Added Login and Registration Pages with Firebase Auth and Firestore Integration

---
+++ src/views/Register.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="page-container">
+		<RegisterForm />
+	</div>
+</template>
+
+<script>
+import RegisterForm from "@/components/RegisterForm.vue";
+
+export default {
+	name: "RegisterView",
+	components: {
+		RegisterForm,
+	},
+};
+</script>
+
+<style scoped>
+.page-container {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	min-height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 20px;
+}
+</style>
