d34cd6ff2f400b19be3633289a59f06dd52d01ff
Bryan Lam <bryanlam02@gmail.com>
2025-04-06 22:23:05 +0800

    fixed firebase initialisation error

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,16 @@ import { getAuth } from "firebase/auth";
 import.meta.env;
 
 // Use import.meta.env to access environment variables:
-    // Create the .env file in the root folder (intern_tracker, NOT src)
-    // copy + paste the details from Telegram
+// Create the .env file in the root folder (intern_tracker, NOT src)
+// copy + paste the details from Telegram
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
